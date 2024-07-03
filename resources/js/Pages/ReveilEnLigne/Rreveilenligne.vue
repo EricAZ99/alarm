3cbcb650337bba141560293cbe71491c550ec38c
josue.tohon <josue.tohon@chem.loc>
2024-07-03 12:26:19 +0200

    la sonnerie

--- resources/js/Pages/ReveilEnLigne/Rreveilenligne.vue
+++ resources/js/Pages/ReveilEnLigne/Rreveilenligne.vue
@@ -1,24 +1,100 @@
 <script setup>
+import { ref, watch } from "vue"
+
 const props = defineProps({
     heure: Number,
     minute: Number,
     alarmName: String
 })
+
+let differenceHours = ref(0)
+let differenceMinutes = ref(0)
+let differenceSeconds = ref(0)
+let alarmSwitch = ref(null)
+// let alarmSound = document.getElementById('alarmSound')
+
+
+
+
+function updateTime() {
+    clearInterval(alarmSwitch)
+
+    // Obtenir l'heure actuelle
+    alarmSwitch = setInterval(() => {
+        let alarmSound = document.getElementById('alarmSound');
+        let now = new Date();
+        let currentHour = now.getHours();
+        let currentMinutes = now.getMinutes();
+        let currentSeconds = now.getSeconds();
+
+        // Convertir alarmeTime en objet Date
+
+        let alarmeHour = parseInt(props.heure, 10);
+        let alarmeMinutes = parseInt(props.minute, 10);
+        let alarmeSeconds = parseInt(0, 10);
+
+        // Créer des objets Date pour l'heure actuelle et l'heure d'alarme
+        let currentDate = new Date();
+        currentDate.setHours(currentHour, currentMinutes, currentSeconds, 0); // Ignorer les millisecondes pour la précision
+
+        let alarmDate = new Date();
+        alarmDate.setHours(alarmeHour, alarmeMinutes, alarmeSeconds, 0);
+
+        // Calculer la différence en millisecondes
+        let differenceMs = alarmDate.getTime() - currentDate.getTime();
+
+        if (differenceMs < 0) {
+            alarmDate.setDate(alarmDate.getDate() + 1); // Ajouter un jour à l'heure d'alarme
+            differenceMs = alarmDate.getTime() - currentDate.getTime();
+        }
+        // Convertir la différence en heures, minutes et secondes
+        differenceHours.value = Math.floor(differenceMs / (1000 * 60 * 60));
+        differenceMinutes.value = Math.floor((differenceMs % (1000 * 60 * 60)) / (1000 * 60));
+        differenceSeconds.value = Math.floor((differenceMs % (1000 * 60)) / 1000);
+
+        
+        if (differenceHours.value == 0 && differenceMinutes.value == 0 && differenceSeconds.value == 0) {
+            clearInterval(alarmSwitch)
+            alarmSound.play()
+        }
+
+    }, 1000);
+}
+
+updateTime()
+
+// watch(() => props.minute, (oldValue) => {
+//   console.log(`Prop 'myProp' a changé de ${oldValue}`);
+// });
+
+const startAlarm = () => {
+
+}
 </script>
 
 <template>
-    <div class="fixed right-0 left-0 top-0 bottom-0 bg-[#f2f7ff] flex justify-center">
-        <div class="min-w-[20rem] w-[40%] bordser border-red-500 flex flex-col items-center">
-            <div
-                class="w-[60%] aspect-square rounded-full border-[.6rem] border-red-500 flex flex-col justify-center items-center">
-                <h2>{{ alarmName }}</h2>
-                <div class="text-[6rem] flex items-center justify-center">
-                    <span>{{ heure }}</span>
-                    <span class="animate-pulse">:</span>
-                    <span>{{ minute }}</span>
-                </div>
+    <div class="min-w-[20rem] w-[40%] bordser border-red-500 flex flex-col items-center">
+        <div id="alarmBorder"
+            class="w-[60%] aspect-square rounded-full border-[.6rem] border-red-500 flex flex-col justify-center items-center">
+            <h2>{{ alarmName }}</h2>
+            <div class="text-[5rem] flex items-center justify-center">
+                <span>{{ differenceHours < 10 ? '0' + differenceHours : differenceHours }}</span>
+                        <span class="animate-pulse">:</span>
+                        <span>{{ differenceMinutes < 10 ? '0' + differenceMinutes : differenceMinutes }}</span>
+                                <span class="animate-pulse">:</span>
+                                <span>{{ differenceSeconds < 10 ? '0' + differenceSeconds : differenceSeconds }}</span>
+            </div>
+            <div>
+                <span>{{ heure }}</span>
+                <span class="animate-pulse">:</span>
+                <span>{{ minute }}</span>
+            </div>
+            <div>
+                <!-- <button @click="startAlarm()">Démarrer</button> -->
+                <audio id="alarmSound">
+                    <source src="../../../../storage/app/public/alame.wav" type="audio/mpeg" preload="auto">
+                </audio>
             </div>
         </div>
-
     </div>
 </template>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -50,33 +50,38 @@ onMounted(() => {
 
 function dark_mode() {
     let main_container = document.getElementById("main_container");
+    let alarmBorder = document.getElementById("alarmBorder");
 
     if (main_container.classList.contains("bg-[#f2f7ff]")) {
         main_container.classList.remove("bg-[#f2f7ff]");
         main_container.classList.add("bg-gray-900");
         main_container.classList.add("text-gray-200");
         main_container.style.transition = '1s';
+        alarmBorder.classList.remove("border-red-500");
+        alarmBorder.classList.add("border-blue-500");
     } else {
         main_container.classList.add("bg-[#f2f7ff]");
         main_container.classList.remove("bg-gray-900");
         main_container.classList.remove("text-gray-200");
+        alarmBorder.classList.remove("border-blue-500");
+        alarmBorder.classList.add("border-red-500");
     }
 }
 </script>
 <template>
     <div id="main_container"
         class="min-w-[25rem] h-dvh bg-[#f2f7ff] text-[#394e6a] flex flex-col items-center font-medium text-[1.4rem]">
-        <nav class="flex justify-between items-center w-full p-4 bordedr border-red-500">
+        <nav class="flex justify-between items-center w-full p-4">
             <div class="flex gap-4">
                 <div class=" relative">
                     <span class="cursor-pointer" @click="menu = !menu">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6 h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                             <path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
                         </svg>
                     </span>
                     <div v-if="menu"
                         class="absolute border border-red-500 w-[12rem] h-[10rem] flex justify-center text-[1rem] font-normal rounded-2xl">
-                        Réveil en ligne
+                        Menu du réveil
                     </div>
                 </div>
 
@@ -88,7 +93,8 @@ function dark_mode() {
                 </span>
             </div>
             <div class="text-[2rem] font-bold">Réveil en ligne</div>
-            <div class="flex gap-4">
+            <div class="flex gap-4 *:cursor-pointer">
+                <!-- Dark mode button -->
                 <span @click="dark_mode">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-6 w-6">
                         <path
@@ -104,151 +110,153 @@ function dark_mode() {
                 </span>
             </div>
         </nav>
-        <div class="min-w-[20rem] w-[40%] flex flex-col justify-center items-center bordder border-red-500">
-            <div>
-                {{ nowDate }}
+        <div class="h-full w-full flex justify-center">
+
+            <div v-if="homeClose" class="min-w-[20rem] w-[40%] flex flex-col justify-center items-center">
+                <div>
+                    {{ nowDate }}
+                </div>
+                <div class="text-[4rem] font-bold">
+                    {{ nowTime }}
+                </div>
+                <form @submit.prevent="setAlarm()" class="w-full flex flex-col gap-8">
+
+                    <div class="flex flex-col">
+                        <p>Définir l'heure de l'alarme</p>
+                        <div class="flex justify-between text-[1rem]">
+                            <div class="w-[48%]">
+                                <label for="heure">
+                                    Heures
+                                </label>
+                                <select name="heure" id="heure" v-model="heure"
+                                    class="w-full rounded shadow-sm border-gray-200 text-black" required>
+                                    <option value="00">00</option>
+                                    <option value="01">01</option>
+                                    <option value="02">02</option>
+                                    <option value="03">03</option>
+                                    <option value="04">04</option>
+                                    <option value="05">05</option>
+                                    <option value="06">06</option>
+                                    <option value="07">07</option>
+                                    <option value="08">08</option>
+                                    <option value="09">09</option>
+                                    <option value="10">10</option>
+                                    <option value="11">11</option>
+                                    <option value="12">12</option>
+                                    <option value="13">13</option>
+                                    <option value="14">14</option>
+                                    <option value="15">15</option>
+                                    <option value="16">16</option>
+                                    <option value="17">17</option>
+                                    <option value="18">18</option>
+                                    <option value="19">19</option>
+                                    <option value="20">20</option>
+                                    <option value="21">21</option>
+                                    <option value="22">22</option>
+                                    <option value="23">23</option>
+                                </select>
+                            </div>
+                            <div class="w-[48%]">
+                                <label for="minute">
+                                    Minutes
+                                </label>
+                                <select name="minute" id="minute" v-model="minute"
+                                    class="w-full rounded shadow-sm border-gray-200 text-black" required>
+                                    <option value="00">00</option>
+                                    <option value="01">01</option>
+                                    <option value="02">02</option>
+                                    <option value="03">03</option>
+                                    <option value="04">04</option>
+                                    <option value="05">05</option>
+                                    <option value="06">06</option>
+                                    <option value="07">07</option>
+                                    <option value="08">08</option>
+                                    <option value="09">09</option>
+                                    <option value="10">10</option>
+                                    <option value="11">11</option>
+                                    <option value="12">12</option>
+                                    <option value="13">13</option>
+                                    <option value="14">14</option>
+                                    <option value="15">15</option>
+                                    <option value="16">16</option>
+                                    <option value="17">17</option>
+                                    <option value="18">18</option>
+                                    <option value="19">19</option>
+                                    <option value="20">20</option>
+                                    <option value="21">21</option>
+                                    <option value="22">22</option>
+                                    <option value="23">23</option>
+                                    <option value="24">24</option>
+                                    <option value="25">25</option>
+                                    <option value="26">26</option>
+                                    <option value="27">27</option>
+                                    <option value="28">28</option>
+                                    <option value="29">29</option>
+                                    <option value="30">30</option>
+                                    <option value="31">31</option>
+                                    <option value="32">32</option>
+                                    <option value="33">33</option>
+                                    <option value="34">34</option>
+                                    <option value="35">35</option>
+                                    <option value="36">36</option>
+                                    <option value="37">37</option>
+                                    <option value="38">38</option>
+                                    <option value="39">39</option>
+                                    <option value="40">40</option>
+                                    <option value="41">41</option>
+                                    <option value="42">42</option>
+                                    <option value="43">43</option>
+                                    <option value="44">44</option>
+                                    <option value="45">45</option>
+                                    <option value="46">46</option>
+                                    <option value="47">47</option>
+                                    <option value="48">48</option>
+                                    <option value="49">49</option>
+                                    <option value="50">50</option>
+                                    <option value="51">51</option>
+                                    <option value="52">52</option>
+                                    <option value="53">53</option>
+                                    <option value="54">54</option>
+                                    <option value="55">55</option>
+                                    <option value="56">56</option>
+                                    <option value="57">57</option>
+                                    <option value="58">58</option>
+                                    <option value="59">59</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col gap-8">
+                        <div>
+                            <p>Son de l'alarme</p>
+                            <div class="">
+                                <select name="ringtone" id="ringtone"
+                                    class="w-full rounded shadow-sm border-gray-200 text-black" required>
+                                    <option value="00">Ring</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div>
+                            <p>Nom de l'alarme</p>
+                            <div class="">
+                                <input name="alarmName" id="alarmName" v-model="alarmName"
+                                    class="w-full rounded shadow-sm border-gray-200 text-black" required />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col gap-8 mt-8">
+                        <button type="submit" id="btn" class="border py-2 bg-blue-600 text-white">Régler une alarme</button>
+                    </div>
+
+                </form>
             </div>
-            <div class="text-[4rem] font-bold">
-                {{ nowTime }}
-            </div>
-            <form @submit.prevent="setAlarm()" class="w-full flex flex-col gap-8">
-
-                <div class="flex flex-col">
-                    <p>Définir l'heure de l'alarme</p>
-                    <div class="flex justify-between text-[1rem]">
-                        <div class="w-[48%]">
-                            <label for="heure">
-                                Heures
-                            </label>
-                            <select name="heure" id="heure" v-model="heure"
-                                class="w-full rounded shadow-sm border-gray-200 text-black" required>
-                                <option value="00">00</option>
-                                <option value="01">01</option>
-                                <option value="02">02</option>
-                                <option value="03">03</option>
-                                <option value="04">04</option>
-                                <option value="05">05</option>
-                                <option value="06">06</option>
-                                <option value="07">07</option>
-                                <option value="08">08</option>
-                                <option value="09">09</option>
-                                <option value="10">10</option>
-                                <option value="11">11</option>
-                                <option value="12">12</option>
-                                <option value="13">13</option>
-                                <option value="14">14</option>
-                                <option value="15">15</option>
-                                <option value="16">16</option>
-                                <option value="17">17</option>
-                                <option value="18">18</option>
-                                <option value="19">19</option>
-                                <option value="20">20</option>
-                                <option value="21">21</option>
-                                <option value="22">22</option>
-                                <option value="23">23</option>
-                            </select>
-                        </div>
-                        <div class="w-[48%]">
-                            <label for="minute">
-                                Minutes
-                            </label>
-                            <select name="minute" id="minute" v-model="minute"
-                                class="w-full rounded shadow-sm border-gray-200 text-black" required>
-                                <option value="00">00</option>
-                                <option value="01">01</option>
-                                <option value="02">02</option>
-                                <option value="03">03</option>
-                                <option value="04">04</option>
-                                <option value="05">05</option>
-                                <option value="06">06</option>
-                                <option value="07">07</option>
-                                <option value="08">08</option>
-                                <option value="09">09</option>
-                                <option value="10">10</option>
-                                <option value="11">11</option>
-                                <option value="12">12</option>
-                                <option value="13">13</option>
-                                <option value="14">14</option>
-                                <option value="15">15</option>
-                                <option value="16">16</option>
-                                <option value="17">17</option>
-                                <option value="18">18</option>
-                                <option value="19">19</option>
-                                <option value="20">20</option>
-                                <option value="21">21</option>
-                                <option value="22">22</option>
-                                <option value="23">23</option>
-                                <option value="24">24</option>
-                                <option value="25">25</option>
-                                <option value="26">26</option>
-                                <option value="27">27</option>
-                                <option value="28">28</option>
-                                <option value="29">29</option>
-                                <option value="30">30</option>
-                                <option value="31">31</option>
-                                <option value="32">32</option>
-                                <option value="33">33</option>
-                                <option value="34">34</option>
-                                <option value="35">35</option>
-                                <option value="36">36</option>
-                                <option value="37">37</option>
-                                <option value="38">38</option>
-                                <option value="39">39</option>
-                                <option value="40">40</option>
-                                <option value="41">41</option>
-                                <option value="42">42</option>
-                                <option value="43">43</option>
-                                <option value="44">44</option>
-                                <option value="45">45</option>
-                                <option value="46">46</option>
-                                <option value="47">47</option>
-                                <option value="48">48</option>
-                                <option value="49">49</option>
-                                <option value="50">50</option>
-                                <option value="51">51</option>
-                                <option value="52">52</option>
-                                <option value="53">53</option>
-                                <option value="54">54</option>
-                                <option value="55">55</option>
-                                <option value="56">56</option>
-                                <option value="57">57</option>
-                                <option value="58">58</option>
-                                <option value="59">59</option>
-                            </select>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="flex flex-col gap-8">
-                    <div>
-                        <p>Son de l'alarme</p>
-                        <div class="">
-                            <select name="ringtone" id="ringtone"
-                                class="w-full rounded shadow-sm border-gray-200 text-black" required>
-                                <option value="00">Ring</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div>
-                        <p>Nom de l'alarme</p>
-                        <div class="">
-                            <input name="alarmName" id="alarmName" v-model="alarmName"
-                                class="w-full rounded shadow-sm border-gray-200 text-black" required />
-                        </div>
-                    </div>
-                </div>
-
-                <div class="flex flex-col gap-8 mt-8">
-                    <button type="submit" id="btn" class="border py-2 bg-blue-600 text-white">Régler une alarme</button>
-                </div>
-
-            </form>
+            <Rreveilenligne v-if="reveilSwitch" class="" :heure="heure" :minute="minute" :alarmName="alarmName" />
         </div>
 
         <!-- Imported files -->
-        <div class="min-w-[20rem] w-[40%]">
-            <Rreveilenligne v-if="reveilSwitch" class="" :heure="heure" :minute="minute" :alarmName="alarmName" />
-        </div>
+
     </div>
 </template>
 
@@ -260,7 +268,8 @@ export default {
             heure: null,
             minute: null,
             timeRunner: null,
-            reveilSwitch: false
+            reveilSwitch: false,
+            homeClose: true,
         }
     },
     methods: {
@@ -275,6 +284,7 @@ export default {
                 alert("Veuillez renseigner le temps du réveil");
             } else {
                 this.reveilSwitch = true
+                this.homeClose = false
             }
         }
     },
